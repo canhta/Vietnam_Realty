af8410f440fe88da6a80af3da0acd9e3a4dd2c10
OppositeIsNo1 <huynh.tt161917@sis.hust.edu.vn>
2018-12-20 12:41:40 +0700

    aaa

--- test.js
+++ test.js
@@ -1,9 +1,14 @@
 var express = require("express");
 
 var app = express();
+app.set('view engine', 'ejs');  
+app.set('views','./views');
+app.use(express.static('public'));
+
+
 app.get("/", function(req, res){
    // res.render("tran trung huynh");
-    res.render();
+    res.render('test');
 })
 
 app.listen(9000, function(){
