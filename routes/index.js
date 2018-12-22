d388a9aa60067c59a9e13cc616df5eac5b0b116c
Torrient <hoangthuyen1998@gmail.com>
2018-12-22 20:27:42 +0700

    edit logo

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("mains/home", { title: "Express" });
+  res.render("mains/sell/postSell", { title: "Express" });
 });
 
 module.exports = router;
