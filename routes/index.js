f9ab6cda15eed1093d284631a8bc08b60fd070f7
Ducvoccer <ntduc250798@gmail.com>
2018-12-22 19:16:24 +0700

    abc

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,6 @@ const express = require("express");
 var router = express.Router();
 router.get("/", (req, res) => res.render("mains/home"));
 
+router.get("/test", (req, res) => res.render("mains/sell/postSell"));
+
 module.exports = router;
