17b8842cd32ed8d6ed7536932abab530df51eb4a
OppositeIsNo1 <huynh.tt161917@sis.hust.edu.vn>
2018-12-22 20:43:42 +0700

    rent

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 var router = express.Router();
-router.get("/", (req, res) => res.render("mains/sell/postSell.ejs"));
+router.get("/", (req, res) => res.render("mains/rent/postRent.ejs"));
 
 
 router.get("/test", (req, res) => res.render("mains/sell/detailSell"));
