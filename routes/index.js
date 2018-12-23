2138bb3ea1ef77ae2bc670e69080afb20d6dcadd
Torrient <hoangthuyen1998@gmail.com>
2018-12-23 12:58:29 +0700

    Layout

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 
 router.get("/", function(req, res, next) {
-  res.render("mains/find/listFind", { title: "Express" });
+  res.render("mains/home", { title: "Express" });
 
 });
 
