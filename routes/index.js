2c58210c157cd9b08495d083ead011b4691d6f9d
OppositeIsNo1 <huynh.tt161917@sis.hust.edu.vn>
2018-12-23 11:27:51 +0700

    abc

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,11 @@
 const express = require("express");
 var router = express.Router();
-<<<<<<< HEAD
-router.get("/", (req, res) => res.render("mains/rent/postRent.ejs"));
+router.get("/", (req, res) => res.render("mains/find/postFind.ejs"));
 
 
 router.get("/test", (req, res) => res.render("mains/sell/detailSell"));
 
-=======
->>>>>>> master
+
 /* GET home page. */
 router.get("/", (req, res, next) => {
   console.log("Hello HOME");
