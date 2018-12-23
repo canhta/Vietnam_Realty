0027de4a4bb2a2eb3b3ba5555bd6c1f334fbc827
Ducvoccer <ntduc250798@gmail.com>
2018-12-23 12:12:37 +0700

    fix validateSellInput

--- routes/api/sell.js
+++ routes/api/sell.js
@@ -89,7 +89,7 @@ router.post("/", (req, res, next) => {
     return res.status(400).json(errors);
   }
   const newSell = new Sell({
-    user: req.session.id,
+    //user: req.session.id,
     hinhThuc: req.body.hinhThuc,
     loai: req.body.loai,
     adress: {
